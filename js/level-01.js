81f93980c417c1a749bb61bb9dc9d134b4f8fbb5
James <jamesmalcolm444@gmail.com>
2024-03-04 17:36:50 +0000

    Transcompiled JS from TS

---
+++ js/level-01.js
@@ -0,0 +1,37 @@
+"use strict";
+//
+// Lenore
+//
+// Wait for all resources to be loaded before executing
+window.addEventListener("load", main);
+// Canvas API
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+// Player Object
+const player = {
+    x: 100, // X Pos
+    y: 100, // Y Pos
+    width: 100, // Width
+    height: 100 // Height
+};
+// Starting function - Setups up the game
+function main() {
+    // Set canvas size to viewport size
+    canvas.width = innerWidth;
+    canvas.height = innerHeight;
+    // Start game loop
+    requestAnimationFrame(game_loop);
+}
+// Executed every frame
+function game_loop() {
+    // Clear previous frame
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    // Render Background 
+    context.fillStyle = "black";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    // Render Player
+    context.fillStyle = "white";
+    context.fillRect(player.x, player.y, player.width, player.height);
+    // Draw next frame
+    requestAnimationFrame(game_loop);
+}
