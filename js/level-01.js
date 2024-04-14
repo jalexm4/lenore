e8f42135baded9f618b356d6640758b3a80a2d8f
James <jamesmalcolm444@gmail.com>
2024-04-14 19:52:02 +0100

    Level supports modules and user input

--- js/level-01.js
+++ js/level-01.js
@@ -1,24 +1,40 @@
-"use strict";
 //
-// Lenore
+// Lenore - Level 1
+// 2D Sidescrolling Game
+// MIT License
 //
+import { handleKeyDown, handleKeyUp } from "./event_handlers.js"; // User Input Event Listeners
 // Wait for all resources to be loaded before executing
 window.addEventListener("load", main);
+// --- Constants ---
+const GRAVITY = 1; // Downward force
 // Canvas API
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 // Player Object
 const player = {
-    x: 100, // X Pos
-    y: 100, // Y Pos
+    x: 100, // 2D space X coordinate (Starting Pos)
+    y: 100, // 2D space Y coordinate (Starting Pos)
     width: 100, // Width
-    height: 100 // Height
+    height: 100, // Height
+    x_velocity: 0, // Movement to apply on X axis
+    y_velocity: 0, // Movement to apply on Y axis
+};
+// Which keys are currently being pressed for a frame
+const keys = {
+    a: false,
+    d: false,
+    space: false,
 };
 // Starting function - Setups up the game
 function main() {
     // Set canvas size to viewport size
-    canvas.width = innerWidth;
-    canvas.height = innerHeight;
+    canvas.width = window.innerWidth;
+    ;
+    canvas.height = window.innerHeight;
+    // Start User Input Event Listeners
+    handleKeyDown(keys);
+    handleKeyUp(keys);
     // Start game loop
     requestAnimationFrame(game_loop);
 }
--- ts/level-01.ts
+++ ts/level-01.ts
@@ -1,9 +1,26 @@
 //
-// Lenore
+// Lenore - Level 1
+// 2D Sidescrolling Game
+// MIT License
 //
+
+// --- Attribution  ----
+//
+// - Background
+// -- Oak Woods Tileset (brullov) @ https://brullov.itch.io/oak-woods
+//
+// - Player
+// -- Fantasy Knight (aamatniekss) @ https://aamatniekss.itch.io/fantasy-knight-free-pixelart-animated-character
+
+// --- Imports ---
+import { Keys } from "./interfaces.js";                             // Interfaces - Only for TypeScript
+import { handleKeyDown, handleKeyUp } from "./event_handlers.js";   // User Input Event Listeners
 
 // Wait for all resources to be loaded before executing
 window.addEventListener("load", main);
+
+// --- Constants ---
+const GRAVITY = 1;  // Downward force
 
 // Canvas API
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
@@ -11,19 +28,32 @@ const context = canvas.getContext("2d") as CanvasRenderingContext2D;
 
 // Player Object
 const player = {
-    x: 100,         // X Pos
-    y: 100,         // Y Pos
+    x: 100,         // 2D space X coordinate (Starting Pos)
+    y: 100,         // 2D space Y coordinate (Starting Pos)
     width: 100,     // Width
-    height: 100     // Height
+    height: 100,    // Height
+
+    x_velocity: 0,  // Movement to apply on X axis
+    y_velocity: 0,  // Movement to apply on Y axis
 };
 
+// Which keys are currently being pressed for a frame
+const keys: Keys = {
+    a: false,
+    d: false,
+    space: false,
+};
 
 // Starting function - Setups up the game
 function main()
 {   
     // Set canvas size to viewport size
-    canvas.width = innerWidth;
-    canvas.height = innerHeight;
+    canvas.width = window.innerWidth;;
+    canvas.height = window.innerHeight;
+
+    // Start User Input Event Listeners
+    handleKeyDown(keys);
+    handleKeyUp(keys);
 
     // Start game loop
     requestAnimationFrame(game_loop);
